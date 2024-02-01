a39bdee42e36ab99fa8c5de014038b17473e1c9d
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2024-02-01 10:09:33 +0100

    feature modules

--- my-demo/src/app/app.component.ts
+++ my-demo/src/app/app.component.ts
@@ -5,10 +5,14 @@ import { Component } from '@angular/core';
   template: `
     <h1>Welcome to {{title}}!</h1>
 
+    <button type="button" routerLink="/customers">Customers</button>
+    <button type="button" routerLink="/orders">Orders</button>
+    <button type="button" routerLink="">Home</button>
+
     <router-outlet></router-outlet>
   `,
   styles: []
 })
 export class AppComponent {
-  title = 'my-demo';
+  title = 'Demo';
 }
---
+++ my-demo/src/app/orders/orders.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-orders',
+  template: `
+    <p>
+      orders works!
+    </p>
+  `,
+  styles: ``
+})
+export class OrdersComponent {
+
+}
